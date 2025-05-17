fa946d4c03b09830cec187263f9b2b034371c070
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-05-17 04:08:49 -0300

    creates structure for the view

---
+++ app/App.tsx
@@ -0,0 +1,13 @@
+import { View, StyleSheet} from "react-native";
+import { Button, Input } from '@rneui/themed';
+
+
+export default function App() {
+  return (
+    <View style={{display: "flex", flex: 1, justifyContent: "center", alignItems: "center"}}>
+       <Input placeholder="Joanzinho" label="Nome"/>
+       <Input placeholder="Senha" label="Senha" rightIcon={{type: 'MaterialIcons', name: 'visibility'}} />
+       <Button title="Solid" />
+    </View>
+  );
+}
