7f378bcbecfc2549c6b9db6ee194bd3dd5ed9f5f
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-05-18 19:19:51 -0300

    Create component card area

---
+++ src/app/components/cardArea/index.tsx
@@ -0,0 +1,48 @@
+import { Card, Text } from "@rneui/themed";
+import { View, StyleSheet } from "react-native";
+import ColorArea from "../colorArea";
+
+interface CardAreaProps {
+  title: string;
+  dimension: string;
+  color: string
+}
+
+export default function CardArea({ title, dimension, color }: CardAreaProps) {
+  return (
+    <Card containerStyle={styles.container}>
+      <Card.Title style={styles.title}>{title}</Card.Title>
+      <Text style={styles.text}>Dimensão: {dimension}m²</Text>
+      <View style={styles.row}>
+        <Text style={styles.text}>Cor da Área</Text>
+        <View style={styles.colorWrapper}>
+          <ColorArea color={color}/>
+        </View>
+      </View>
+    </Card>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "70%",
+    borderRadius: 5,
+    backgroundColor: "#8D6E63",
+  },
+  title: {
+    fontSize: 25,
+    color: "white",
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 7,
+  },
+  colorWrapper: {
+    marginLeft: 5
+  },
+});
