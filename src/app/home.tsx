9940a6a5dfd5749341449cf204d8f5d02e7d9134
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-05-18 19:21:12 -0300

    create user home

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,3 +1,21 @@
+import { View, StyleSheet } from "react-native";
+import CustomButtom from "./components/buttom";
+import CardArea from "./components/cardArea";
+
 export default function Home() {
-    
+    return(
+        <View style={styles.container}>
+            <CustomButtom title="Nova Área" icon="plus" type="entypo"/>
+            <CardArea title="Área 1" dimension="60" color="#CFB8B8"/>
+            <CardArea title="Área 2" dimension="70" color="#CFB858"/>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
