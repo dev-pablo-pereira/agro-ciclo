e369248d6e57abf6fcf1ff8b62b295b8f013738a
dev-pablo-pereira <pablitogit@gmail.com>
2025-05-18 21:31:14 -0300

    #8 Feature - Criar tela home do usuário (#12)
    * remove file
    
    * Create component card area
    
    * Create component colorArea
    
    * create user home
    
    * add navigate to home
    
    * Add side menu
    
    * add fake datas and flatlist
    
    * add margin
    
    * change background
    
    * add paddingBottom
    
    * adjust margin bottom
    
    * hide login header
    
    ---------
    
    Co-authored-by: Pablo Andrei Pereira <pabloandrei0906@gmail.com>

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,3 +1,54 @@
+import { View, StyleSheet } from "react-native";
+import CustomButtom from "./components/buttom";
+import CardArea from "./components/cardArea";
+import { FlatList } from "react-native";
+
+const AREAS = [
+  { id: "1", title: "Área 1", dimension: "60", color: "#CFB8B8" },
+  { id: "2", title: "Área 2", dimension: "70", color: "#CFB858" },
+  { id: "3", title: "Área 1", dimension: "60", color: "#CFB8B8" },
+  { id: "4", title: "Área 2", dimension: "70", color: "#CFB858" },
+  { id: "5", title: "Área 1", dimension: "60", color: "#CFB8B8" },
+  { id: "6", title: "Área 2", dimension: "70", color: "#CFB858" },
+];
+
 export default function Home() {
-    
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerButtom}>
+        <CustomButtom title="Nova Área" icon="plus" type="entypo" />
+      </View>
+
+      <FlatList
+        data={AREAS}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <CardArea
+            title={item.title}
+            dimension={item.dimension}
+            color={item.color}
+          />
+        )}
+        contentContainerStyle={styles.list}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    backgroundColor: "#F5F5F5",
+    paddingBottom: 30
+  },
+  containerButtom: {
+    alignItems: 'center',
+    width: '100%'
+  },
+  list: {
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+});
