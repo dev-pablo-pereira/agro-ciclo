07f0eda8955ea95658ffd8060936f0f17d939c86
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-05-21 18:59:24 -0300

    Add button with icon and onpress as props

--- src/app/components/cardLocation/index.tsx
+++ src/app/components/cardLocation/index.tsx
@@ -1,23 +1,36 @@
-import { Card, Text } from "@rneui/themed";
+import { Button, Card, Icon, Text } from "@rneui/themed";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { View } from "react-native";
 
 interface CardLocationProps {
   lat: number;
   long: number;
+  onPress?: () => void
 }
 
-export default function CardLocation({ lat, long }: CardLocationProps) {
+export default function CardLocation({ lat, long, onPress}: CardLocationProps) {
   return (
     <Card containerStyle={styles.container}>
-      <Text style={styles.text}>Lat: {lat}</Text>
-      <Text style={styles.text}>Long: {long}</Text>
+      <View style={styles.row}>
+        <View>
+          <Text style={styles.text}>Lat: {lat}</Text>
+          <Text style={styles.text}>Long: {long}</Text>
+        </View>
+        <Button
+        onPress={onPress}
+        type="clear"
+        icon={{
+          name:"trash-2",
+          type:"feather",
+          color:'#C25757'
+        }}/>
+      </View>
     </Card>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
     width: "70%",
     borderRadius: 5,
     backgroundColor: "#8D6E63",
@@ -25,5 +38,10 @@ const styles = StyleSheet.create({
   text: {
     color: "white",
     fontSize: 20,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'space-between'
   }
 });
