1d03faa7e6f621f56d0b956f173399ac093448b3
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-05-24 18:30:07 -0300

    create location and area mocks

---
+++ src/mocks/Locations.ts
@@ -0,0 +1,32 @@
+import { CardLocationProps } from "../components/cardLocation";
+
+export const locations: CardLocationProps[] = [
+  {
+    lat: 30,
+    long: 90,
+  },
+  {
+    lat: 31,
+    long: 91,
+  },
+  {
+    lat: 32,
+    long: 92,
+  },
+  {
+    lat: 33,
+    long: 93,
+  },
+  {
+    lat: 34,
+    long: 94,
+  },
+  {
+    lat: 35,
+    long: 95,
+  },
+  {
+    lat: 36,
+    long: 96,
+  },
+];
