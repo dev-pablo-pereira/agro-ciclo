f2c61c9728a3dc5087fc5409aa2de701302c4214
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-05-31 20:21:47 -0300

    Add schema folder in db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 import { eq } from 'drizzle-orm';
-import { usersTable } from './db/user';
+import { usersTable } from './db/schema/user';
 
 const db = drizzle(`file:${process.env.DB_FILE_NAME!}`);
 
@@ -14,7 +14,7 @@ async function main() {
   await db.insert(usersTable).values(user);
   console.log('New user created!')
 
-  const users = await db.select().from(usersTable);
+  const users = db.select().from(usersTable);
   console.log('Getting all users from the database: ', users)
 
   await db
