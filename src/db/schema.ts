8ae1aff398244e38b773854ca252b9d4a5eaf4cf
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-07 20:57:44 -0300

    add table areas

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,5 +3,12 @@ import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const users = sqliteTable("users", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
-  password: text().notNull().unique()
+  password: text().notNull().unique(),
+});
+
+export const areas = sqliteTable("areas", {
+  id: int().primaryKey({ autoIncrement: true }),
+  id_user: int().references(() => users.id).notNull(),
+  name: text().notNull(),
+  color: text()
 });
