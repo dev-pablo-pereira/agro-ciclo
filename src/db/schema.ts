dcfa62d65324e46958c6bb07a982a3d7cadd8df4
dev-pablo-pereira <pablitogit@gmail.com>
2025-06-08 01:41:05 -0300

    [26] Feature: Criar schema para tabelas (#27)
    * add table areas
    
    * add table coordinates
    
    * add products table
    
    * add harvests table
    
    * add cultivations table
    
    ---------
    
    Co-authored-by: Pablo Andrei Pereira <pabloandrei0906@gmail.com>

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,47 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, sqliteTable, text, } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
-  password: text().notNull().unique()
+  password: text().notNull().unique(),
+});
+
+export const areas = sqliteTable("areas", {
+  id: int().primaryKey({ autoIncrement: true }),
+  id_user: int()
+    .references(() => users.id)
+    .notNull(),
+  name: text().notNull(),
+  color: text(),
+});
+
+export const coordinates = sqliteTable("coordinates", {
+  id: int().primaryKey({ autoIncrement: true }),
+  id_area: int()
+    .references(() => areas.id)
+    .notNull(),
+  latitude: int().notNull(),
+  longitude: int().notNull(),
+});
+
+export const products = sqliteTable("products", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  spacing: int().notNull(),
+  germination: int().notNull(),
+  population_ha: int().notNull(),
+});
+
+export const harvests = sqliteTable("harvests", {
+  id: int().primaryKey({autoIncrement:true}),
+  name: text().notNull(),
+  season: text().notNull(),
+  start: text().notNull(), // ver como adidionar o tipo data
+  end: text().notNull(),
+});
+
+export const cultivations = sqliteTable("cultivations", {
+  id: int().primaryKey({ autoIncrement: true }),
+  id_area: int().references(() => areas.id),
+  id_product: int().references(() => products.id),
 });
