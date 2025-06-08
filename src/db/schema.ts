20558d1653d1d2050915542409037b9265148032
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-07 21:39:15 -0300

    add table coordinates

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,7 +8,18 @@ export const users = sqliteTable("users", {
 
 export const areas = sqliteTable("areas", {
   id: int().primaryKey({ autoIncrement: true }),
-  id_user: int().references(() => users.id).notNull(),
+  id_user: int()
+    .references(() => users.id)
+    .notNull(),
   name: text().notNull(),
-  color: text()
+  color: text(),
+});
+
+export const coordinates = sqliteTable("coordinates", {
+  id: int().primaryKey({ autoIncrement: true }),
+  id_area: int()
+    .references(() => areas.id)
+    .notNull(),
+  latitude: int().notNull(),
+  longitude: int().notNull(),
 });
