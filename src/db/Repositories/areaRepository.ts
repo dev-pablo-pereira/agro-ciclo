01b2a02fccc6b434ce2f5abf3bbd8a17a5f43ed4
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-11 14:57:47 -0300

    add return result

--- src/db/Repositories/areaRepository.ts
+++ src/db/Repositories/areaRepository.ts
@@ -2,13 +2,15 @@ import { db } from "../db";
 import { areas } from "../schema";
 
 export async function createArea(idUser: number, name: string, color: string) {
-  await db.insert(areas).values([
+  const result = await db.insert(areas).values([
     {
       id_user: idUser,
       name: name,
       color: color,
     },
-  ]);
+  ]).returning(); // Retorna os campos do usuário recém-criado
+
+  return result[0]; ;
 }
 export async function allArea(idUser: number) {
   const result = await db.select().from(areas);
