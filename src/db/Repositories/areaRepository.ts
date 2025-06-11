0326f71371b1550e3bf1fb6e1fc75c3a1d31aed8
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-11 15:06:55 -0300

    add query to retrieve areas only from the current user

--- src/db/Repositories/areaRepository.ts
+++ src/db/Repositories/areaRepository.ts
@@ -2,17 +2,23 @@ import { db } from "../db";
 import { areas } from "../schema";
 
 export async function createArea(idUser: number, name: string, color: string) {
-  const result = await db.insert(areas).values([
-    {
-      id_user: idUser,
-      name: name,
-      color: color,
-    },
-  ]).returning(); // Retorna os campos do usuário recém-criado
+  const result = await db
+    .insert(areas)
+    .values([
+      {
+        id_user: idUser,
+        name: name,
+        color: color,
+      },
+    ])
+    .returning(); // Retorna os campos do usuário recém-criado
 
-  return result[0]; ;
+  return result[0];
 }
+
 export async function allArea(idUser: number) {
-  const result = await db.select().from(areas);
+  const result = await db.query.areas.findMany({
+    where: (areas, { eq }) => eq(areas.id_user, idUser),
+  });
   return result;
 }
