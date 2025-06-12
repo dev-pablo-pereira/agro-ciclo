b089dafb77e0746fd7dc65d72585debe2b574864
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-11 21:24:58 -0300

    usa tinycolor2 para conversor para HEX

--- src/components/pickColor/index.tsx
+++ src/components/pickColor/index.tsx
@@ -1,5 +1,7 @@
 import { ColorWheel } from "react-native-color-wheel";
 import { StyleSheet } from "react-native";
+import tinycolor from "tinycolor2";
+
 import useAddColor from "../../states/colorArea";
 
 export default function PickColor() {
@@ -8,7 +10,11 @@ export default function PickColor() {
   return (
     <ColorWheel
       initialColor="#ee0000"
-      onColorChangeComplete={(color) => addColor(color)}
+      onColorChangeComplete={(color) => {
+        const hex = tinycolor(color).toHexString();
+        console.log("Selecionou cor:", hex);
+        addColor(hex);
+      }}
       style={styles.wheel}
     />
   );
