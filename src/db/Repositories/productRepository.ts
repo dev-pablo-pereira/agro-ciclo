26e45e5957130aff2593c91ed443e985a0e773bb
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-14 20:42:37 -0300

    Adds function to insert product

---
+++ src/db/Repositories/productRepository.ts
@@ -0,0 +1,18 @@
+import { db } from "../db";
+import { products } from "../schema";
+
+export async function create(
+  name: string,
+  spacing: number,
+  germination: number,
+  population_ha: number
+) {
+  await db
+    .insert(products)
+    .values({
+      name: name,
+      spacing: spacing,
+      germination: germination,
+      population_ha: population_ha,
+    });
+}
