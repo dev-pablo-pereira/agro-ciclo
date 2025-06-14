3e6c9e95c06b9dcd3fa86458d32b10a64280af63
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-14 20:43:03 -0300

    create new product

--- src/app/product.tsx
+++ src/app/product.tsx
@@ -1,35 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import CustomInput from "../components/input";
 import CustomButtom from "../components/buttom";
+import { create } from "../db/Repositories/productRepository";
 
 export default function Product() {
+  // atributos product
+  const [name, setName] = useState("");
+  const [populationHa, setPopulationHa] = useState<number>();
+  const [spacing, setSpacing] = useState<number>();
+  const [germination, setGermination] = useState<number>();
+
+  //functions
+  const createProduct = () => {
+    if (
+      name !== "" &&
+      populationHa !== undefined &&
+      spacing !== undefined &&
+      germination !== undefined
+    ) {
+      create(name, populationHa, spacing, germination);
+    }
+  };
+
   return (
     <View style={style.container}>
       <CustomInput
         label="Produto"
         placeholder="Ex: Milho"
-        onChangeText={() => {}}
+        onChangeText={(val) => setName(val)}
       />
       <CustomInput
-        label="População por hectare"
+        label="População por ha"
         placeholder="Ex: 50 mil"
-        onChangeText={() => {}}
+        onChangeText={(val) => setPopulationHa(Number(val))}
       />
       <CustomInput
         label="Espaçamento em M"
         placeholder="Ex: 0,80 m"
-        onChangeText={() => {}}
+        onChangeText={(val) => setSpacing(Number(val))}
       />
       <CustomInput
         label="Germinação"
         placeholder="Ex: 85%"
-        onChangeText={() => {}}
+        onChangeText={(val) => setGermination(Number(val))}
       />
       <CustomButtom
         title="Salvar"
         icon="save"
-        onPress={() => console.log("produto")}
+        onPress={() => createProduct()}
       />
     </View>
   );
