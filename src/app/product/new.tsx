884162a02ea70875d924cf8b386cc5016aae25a4
dev-pablo-pereira <pablitogit@gmail.com>
2025-06-15 14:09:33 -0300

    [31] Feature - CRUD de produtos (#34)
    * create new screen product
    
    * Creates screen structure
    
    * Create style for the container
    
    * Adds function to insert product
    
    * create new product
    
    * create index
    
    * create product diretory
    
    * add get all products
    
    * Create card for product information and retrieve all products
    
    * add delete
    
    * delata e filtra os produtos já excluídos da visualização
    
    * Adds product information search
    
    * Add button to redirect to edit
    
    * Adds product values ​​to the input's value property
    
    * add product edit
    
    * Adds function to check changes before updating
    
    ---------
    
    Co-authored-by: Pablo Andrei Pereira <pabloandrei0906@gmail.com>

---
+++ src/app/product/new.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import CustomInput from "../../components/input";
+import CustomButtom from "../../components/buttom";
+import { create } from "../../db/Repositories/productRepository";
+import { router } from "expo-router";
+
+export default function Index() {
+  // atributos product
+  const [name, setName] = useState("");
+  const [populationHa, setPopulationHa] = useState<number>();
+  const [spacing, setSpacing] = useState<number>();
+  const [germination, setGermination] = useState<number>();
+
+  //functions
+  const createProduct = () => {
+    if (
+      name !== "" &&
+      populationHa !== undefined &&
+      spacing !== undefined &&
+      germination !== undefined
+    ) {
+      create(name, populationHa, spacing, germination);
+      router.push("product")
+    }
+  };
+
+  return (
+    <View style={style.container}>
+      <CustomInput
+        label="Produto"
+        placeholder="Ex: Milho"
+        onChangeText={(val) => setName(val)}
+      />
+      <CustomInput
+        label="População por ha"
+        placeholder="Ex: 50 mil"
+        onChangeText={(val) => setPopulationHa(Number(val))}
+      />
+      <CustomInput
+        label="Espaçamento em M"
+        placeholder="Ex: 0,80 m"
+        onChangeText={(val) => setSpacing(Number(val))}
+      />
+      <CustomInput
+        label="Germinação"
+        placeholder="Ex: 85%"
+        onChangeText={(val) => setGermination(Number(val))}
+      />
+      <CustomButtom
+        title="Salvar"
+        icon="save"
+        onPress={() => createProduct()}
+      />
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+});
