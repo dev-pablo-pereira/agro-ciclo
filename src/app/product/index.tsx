79d4690fb1f2286073d31252a944be8c42849b06
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-14 21:23:48 -0300

    Create card for product information and retrieve all products

--- src/app/product/index.tsx
+++ src/app/product/index.tsx
@@ -1,24 +1,50 @@
 import { Button, Card, Text } from "@rneui/themed";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import CustomButtom from "../../components/buttom";
 import { router } from "expo-router";
+import { allProduct } from "../../db/Repositories/productRepository";
+import { FlatList } from "react-native";
+
+type Product = {
+  id: number;
+  name: string;
+  spacing: number;
+  germination: number;
+  population_ha: number;
+};
 
 export default function index() {
+  //sets
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const allProducts = async () => {
+      const products = await allProduct();
+      setProducts(products);
+    };
+    allProducts();
+  });
+
   return (
     <View>
       <CustomButtom title="Novo" onPress={() => router.push("product/new")} />
-      <Card>
-        <>
-          <Card.Title>Nome Produto</Card.Title>
-          <Text>População: valor</Text>
-          <Text>Espaçamento: valor</Text>
-          <Text>Germinação: valor</Text>
-        </>
-        <>
-          <Button type="solid">Delete</Button>
-        </>
-      </Card>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => (
+          <Card>
+            <>
+              <Card.Title>{item.name}</Card.Title>
+              <Text>População ha: {item.population_ha} mil</Text>
+              <Text>Espaçamento: {item.spacing} m</Text>
+              <Text>Germinação: {item.germination}%</Text>
+            </>
+            <>
+              <Button type="solid">Delete</Button>
+            </>
+          </Card>
+        )}
+      />
     </View>
   );
 }
