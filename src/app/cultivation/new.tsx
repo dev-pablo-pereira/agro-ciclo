8e5e45d57304f21d03adeb1a1a1cca4a86931497
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-15 20:56:46 -0300

    Retrieves all items from the bank and sets them as options for the Picker

--- src/app/cultivation/new.tsx
+++ src/app/cultivation/new.tsx
@@ -1,45 +1,103 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
+import { allProduct } from "../../db/Repositories/productRepository";
+import { allArea } from "../../db/Repositories/areaRepository";
+import useCurrentUser from "../../states/currentUser";
+import { getAll } from "../../db/Repositories/harvestRepository";
+import CustomButtom from "../../components/buttom";
+
+type Product = {
+  id: number;
+  name: string;
+  spacing: number;
+  germination: number;
+  population_ha: number;
+};
+
+type Area = {
+  id: number;
+  name: string;
+  id_user: number;
+  color: string | null;
+};
+
+type Harvest = {
+  id: number;
+  name: string;
+  season: string;
+  start: string;
+  end: string;
+};
 
 export default function New() {
-  const [selectedLanguage, setSelectedLanguage] = useState<string>();
+  const { currentUser } = useCurrentUser();
 
-  const pickerRef = useRef<Picker<string> | null>(null);
+  const [product, setProduct] = useState<string>();
+  const [area, setArea] = useState<string>();
+  const [harvest, setHarvest] = useState<string>();
 
-  const open = () => {
-    pickerRef.current?.focus();
-  };
+  const [listProducts, setListProducts] = useState<Product[]>([]);
+  const [listAreas, setListAreas] = useState<Area[]>([]);
+  const [listHarvests, setListHarvests] = useState<Harvest[]>([]);
 
-  const close = () => {
-    pickerRef.current?.blur();
-  };
+  useEffect(() => {
+    const products = async () => {
+      const listProducts = await allProduct();
+      setListProducts(listProducts);
+    };
+    products();
+  }, []);
+
+  useEffect(() => {
+    const areas = async () => {
+      if (currentUser) {
+        const listAreas = await allArea(currentUser);
+        setListAreas(listAreas);
+      }
+    };
+    areas();
+  }, []);
+
+  useEffect(() => {
+    const harvests = async () => {
+      if (currentUser) {
+        const listHarvests = await getAll();
+        setListHarvests(listHarvests);
+      }
+    };
+    harvests();
+  }, []);
+
   return (
     <View>
       <Picker
-        ref={pickerRef}
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}
+        selectedValue={product}
+        onValueChange={(itemValue) => setProduct(itemValue)}
       >
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
+        {listProducts.map((item) => (
+          <Picker.Item label={item.name} value={item.name} />
+        ))}
       </Picker>
+
       <Picker
-        ref={pickerRef}
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}
+        selectedValue={area}
+        onValueChange={(itemValue) => setArea(itemValue)}
       >
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
+        {listAreas.map((item) => (
+          <Picker.Item label={item.name} value={item.name} />
+        ))}
       </Picker>
+
       <Picker
-        ref={pickerRef}
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}
+        selectedValue={harvest}
+        onValueChange={(itemValue) => setHarvest(itemValue)}
       >
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="JavaScript" value="js" />
+        {listHarvests.map((item) => (
+          <Picker.Item label={item.name} value={item.name} />
+        ))}
       </Picker>
+      <CustomButtom title="teste" onPress={() => console.log(`${product} ${area}, ${harvest}`)} />
     </View>
   );
 }
