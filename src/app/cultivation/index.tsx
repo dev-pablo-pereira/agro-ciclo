4d6ac7272f2027db126517760d1d211eb07cc686
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-16 02:37:42 -0300

    add redirect to edit screen

--- src/app/cultivation/index.tsx
+++ src/app/cultivation/index.tsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import CustomButtom from "../../components/buttom";
 import { router } from "expo-router";
-import { deleteCultivation, getAllCultivations } from "../../db/Repositories/cultivationRepository";
+import {
+  deleteCultivation,
+  getAllCultivations,
+} from "../../db/Repositories/cultivationRepository";
 import { FlatList } from "react-native";
 import { Button, Card, Text } from "@rneui/themed";
 
 type Cultivation = {
-  id_cultivation: number
+  id_cultivation: number;
   productName: string;
   areaName: string;
   harvestName?: string;
@@ -26,10 +29,12 @@ export default function index() {
     cultivations();
   }, []);
 
-  const deleteCul = async (id:number) => {
-    await deleteCultivation(id)
-    setListCultivations((prev) => prev.filter((item) => item.id_cultivation !== id));
-  }
+  const deleteCul = async (id: number) => {
+    await deleteCultivation(id);
+    setListCultivations((prev) =>
+      prev.filter((item) => item.id_cultivation !== id)
+    );
+  };
   return (
     <View>
       <CustomButtom
@@ -40,18 +45,22 @@ export default function index() {
       <FlatList
         data={listCultivations}
         renderItem={({ item }) => (
-          <Card>
-            <Card.Title>{item.productName}</Card.Title>
-            <Text>{item.areaName}</Text>
-             <Text>{item.harvestName}</Text>
-            <CustomButtom
-              title="Estimativa"
-              icon="calculator"
-              type="antdesign"
-              onPress={() => {}}
-            />
-            <Button onPress={() => deleteCul(item.id_cultivation)}>Delete</Button>
-          </Card>
+          <Button onPress={() => router.push(`/cultivation/${item.id_cultivation}`)} type="clear">
+            <Card>
+              <Card.Title>{item.productName}</Card.Title>
+              <Text>{item.areaName}</Text>
+              <Text>{item.harvestName}</Text>
+              <CustomButtom
+                title="Estimativa"
+                icon="calculator"
+                type="antdesign"
+                onPress={() => {}}
+              />
+              <Button onPress={() => deleteCul(item.id_cultivation)}>
+                Delete
+              </Button>
+            </Card>
+          </Button>
         )}
       />
     </View>
