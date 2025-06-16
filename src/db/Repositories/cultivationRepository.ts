8d52dd22f2cf19d03eaa83e61c85c4b7334c5b38
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-16 02:35:15 -0300

    Adds methods to retrieve and edit the crop

--- src/db/Repositories/cultivationRepository.ts
+++ src/db/Repositories/cultivationRepository.ts
@@ -12,6 +12,13 @@ export async function newCultivation(
     .values({ id_area: idArea, id_product: idProduct, id_harvest: idHarvest });
 }
 
+export async function getCultivation(id: number) {
+  const result = await db.query.cultivations.findFirst({
+    where: (cultivations, { eq }) => eq(cultivations.id, id),
+  });
+  return result;
+}
+
 export async function getAllCultivations() {
   const result = await db
     .select({
@@ -26,6 +33,22 @@ export async function getAllCultivations() {
   return result;
 }
 
-export async function deleteCultivation(id:number) {
-  await db.delete(cultivations).where( eq(cultivations.id, id))
+export async function editCultivation(
+  id: number,
+  id_area: number,
+  id_product: number,
+  id_harvest?: number
+) {
+  await db
+    .update(cultivations)
+    .set({
+      id_area: id_area,
+      id_product: id_product,
+      id_harvest: id_harvest,
+    })
+    .where(eq(cultivations.id, id));
+}
+
+export async function deleteCultivation(id: number) {
+  await db.delete(cultivations).where(eq(cultivations.id, id));
 }
