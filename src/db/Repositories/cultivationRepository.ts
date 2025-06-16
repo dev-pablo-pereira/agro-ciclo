1f9ee9052195c9e73ff495095c324e53867e4b89
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-16 01:47:37 -0300

    Add query to retrieve all crops and tables needed for viewing

--- src/db/Repositories/cultivationRepository.ts
+++ src/db/Repositories/cultivationRepository.ts
@@ -1,9 +1,26 @@
 import { eq } from "drizzle-orm";
 import { db } from "../db";
-import { cultivations } from "../schema";
+import { areas, cultivations, harvests, products } from "../schema";
 
-export async function newCultivation(idArea: number, idProduct: number) {
+export async function newCultivation(
+  idArea: number,
+  idProduct: number,
+  idHarvest?: number
+) {
   await db
     .insert(cultivations)
-    .values({ id_area: idArea, id_product: idProduct });
+    .values({ id_area: idArea, id_product: idProduct, id_harvest: idHarvest });
+}
+
+export async function getAllCultivations() {
+  const result = await db
+    .select({
+      productName: products.name,
+      areaName: areas.name,
+    })
+    .from(cultivations)
+    .innerJoin(products, eq(cultivations.id_product, products.id))
+    .innerJoin(areas, eq(cultivations.id_area, areas.id))
+    .innerJoin(harvests, eq(cultivations.id_harvest, harvests.id));
+  return result;
 }
