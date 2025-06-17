368beb96a04a3441c4efb5a7c004655d7fa88cad
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-17 20:40:09 -0300

    add pms column

---
+++ src/drizzle/0003_lyrical_salo.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `products` ADD `pms` integer NOT NULL;
--- src/drizzle/migrations.js
+++ src/drizzle/migrations.js
@@ -4,13 +4,15 @@ import journal from './meta/_journal.json';
 import m0000 from './0000_gigantic_the_hunter.sql';
 import m0001 from './0001_typical_kat_farrell.sql';
 import m0002 from './0002_complex_richard_fisk.sql';
+import m0003 from './0003_lyrical_salo.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
 m0001,
-m0002
+m0002,
+m0003
     }
   }
   
