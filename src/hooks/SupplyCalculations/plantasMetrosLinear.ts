a0250ceeff5f3777454526303f995508ead6f161
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-17 20:35:36 -0300

    create calc SeedsPerLinearMeter

--- src/hooks/SupplyCalculations/plantasMetrosLinear.ts
+++ src/hooks/SupplyCalculations/plantasMetrosLinear.ts
@@ -1,8 +1,16 @@
 import { getProduct } from "../../db/Repositories/productRepository";
 
-export async function PlantPerLinearMeter(id:number) {
-    const data =  await getProduct(id)
-    if (data) {
-        return data.population_ha * data.spacing / 10000 // valor do hectare em m2
-    }
+export async function PlantPerLinearMeter(id: number) {
+  const data = await getProduct(id);
+  if (data) {
+    return (data.population_ha * data.spacing) / 10000; // valor do hectare em m2
+  }
+}
+
+export async function SeedsPerLinearMeter(id: number) {
+  const data = await getProduct(id);
+  let numberPlants = await PlantPerLinearMeter(id);
+  if (data && numberPlants !== undefined) {
+    return (numberPlants * 100) / data.germination;
+  }
 }
