9dc16cef6867abcb0784ba78fa22dece7d693938
Pablo Andrei Pereira <pabloandrei0906@gmail.com>
2025-06-21 18:58:16 -0300

    Creates the style for the product listing screen

--- src/app/product/index.tsx
+++ src/app/product/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Card, Text } from "@rneui/themed";
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import CustomButtom from "../../components/buttom";
 import { router } from "expo-router";
 import {
@@ -8,13 +8,15 @@ import {
   deleteProduct as deleteProductDb,
 } from "../../db/Repositories/productRepository";
 import { FlatList } from "react-native";
+import DeleteButton from "../../components/buttom/delete";
+import EditButton from "../../components/buttom/edit";
 
 type Product = {
   id: number;
   name: string;
   spacing: number;
   germination: number;
-  pms: number
+  pms: number;
   population_ha: number;
 };
 
@@ -37,25 +39,32 @@ export default function index() {
   }, []);
 
   return (
-    <View>
+    <View style={styles.container}>
       <CustomButtom title="Novo" onPress={() => router.push("product/new")} />
       <FlatList
+        style={styles.list}
         data={products}
         renderItem={({ item }) => (
-          <Button type="clear" onPress={() => router.push(`product/${item.id}`)}>
-            <Card>
-              <>
-                <Card.Title>{item.name}</Card.Title>
-                <Text>População ha: {item.population_ha} mil</Text>
-                <Text>Espaçamento: {item.spacing} m</Text>
-                <Text>Germinação: {item.germination}%</Text>
-                <Text>PMS: {item.pms}%</Text>
-              </>
-              <>
-                <Button type="solid" onPress={() => deleteProduct(item.id)}>
-                  Delete
-                </Button>
-              </>
+          <Button
+            type="clear"
+            onPress={() => router.push(`product/${item.id}`)}
+          >
+            <Card containerStyle={styles.card}>
+              <View>
+                <Card.Title style={styles.title}>{item.name}</Card.Title>
+                <Text style={styles.text}>
+                  População ha: {item.population_ha} mil
+                </Text>
+                <Text style={styles.text}>Espaçamento: {item.spacing} m</Text>
+                <Text style={styles.text}>Germinação: {item.germination}%</Text>
+                <Text style={styles.text}>PMS: {item.pms}%</Text>
+              </View>
+              <View style={styles.options}>
+                <DeleteButton onPress={() => deleteProduct(item.id)} />
+                <EditButton
+                  onPress={() => router.push(`/product/${item.id}`)}
+                />
+              </View>
             </Card>
           </Button>
         )}
@@ -63,3 +72,37 @@ export default function index() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  list: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: "#8D6E63",
+    width: "90%",
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 25,
+    color: "white",
+    marginBottom: 6,
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  options: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+    width: "100%",
+  },
+});
