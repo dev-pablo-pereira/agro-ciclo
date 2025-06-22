98a151c259ec8bdc791f58fe62d2993dab63fe76
dev-pablo-pereira <pablitogit@gmail.com>
2025-06-21 23:21:34 -0300

    [46] Feature - Design tela de safras (#52)
    * Add error message
    
    * Add redirect
    
    * create style harvest screen
    
    ---------
    
    Co-authored-by: Pablo Andrei Pereira <pabloandrei0906@gmail.com>

--- src/app/harvest/index.tsx
+++ src/app/harvest/index.tsx
@@ -5,6 +5,9 @@ import { router } from "expo-router";
 import { deleteHavest, getAll } from "../../db/Repositories/harvestRepository";
 import { Button, Card, Text } from "@rneui/themed";
 import { FlatList } from "react-native";
+import { StyleSheet } from "react-native";
+import DeleteButton from "../../components/buttom/delete";
+import EditButton from "../../components/buttom/edit";
 
 type Harvest = {
   id: number;
@@ -31,26 +34,62 @@ export default function index() {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <CustomButtom
         title="Nova Safra"
         icon="save"
         onPress={() => router.push("/harvest/new")}
       />
       <FlatList
+        style={styles.list}
         data={harvests}
         renderItem={({ item }) => (
-          <Button type="clear" onPress={()=> router.push(`/harvest/${item.id}`)}>
-            <Card>
-              <Card.Title>{item.name}</Card.Title>
-              <Text>{item.season}</Text>
-              <Text>{item.start}</Text>
-              <Text>{item.end}</Text>
-              <Button onPress={() => deleteItem(item.id)}>Delete</Button>
-            </Card>
-          </Button>
+          <Card containerStyle={styles.card}>
+            <Card.Title style={styles.title}>{item.name}</Card.Title>
+            <Text style={styles.text}>{item.season}</Text>
+            <Text style={styles.text}>{item.start}</Text>
+            <Text style={styles.text}>{item.end}</Text>
+            <View style={styles.options}>
+              <DeleteButton onPress={() => deleteItem(item.id)} />
+              <EditButton onPress={() => router.push(`/harvest/${item.id}`)} />
+            </View>
+          </Card>
         )}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 20,
+  },
+  list: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: "#8D6E63",
+    width: "90%",
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 25,
+    color: "white",
+    marginBottom: 6,
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  options: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+    width: "100%",
+  },
+});
